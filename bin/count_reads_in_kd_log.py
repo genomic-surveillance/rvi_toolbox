5344d90b9d9b1f5b5d100d78bd7c6463870c8c38
Antonio Marinho <ad45@sanger.ac.uk>
2024-06-06 10:22:55 +0100

    add comments

--- bin/count_reads_in_kd_log.py
+++ bin/count_reads_in_kd_log.py
@@ -3,10 +3,22 @@
 import sys
 from os import path
 
-# Function to parse a file
 def get_read_info(kd_log_file):
+    """
+    Function to parse a file and extract read information.
+
+    Args:
+        kd_log_file (str): Path to the log file
+
+    Returns:
+        list: List of strings containing read information
+    """
+    # Extract meta ID from log file name
     read_info = []
     metaID = path.basename(kd_log_file).replace("_1_kneaddata.log", "")
+
+    # Iterate over each line in the log file, 
+    # if the line contains "READ COUNT", extract number of reads
     with open(kd_log_file, 'r') as kd_log:
         for line in kd_log:
             if "READ COUNT" in line:
@@ -17,9 +29,12 @@ def get_read_info(kd_log_file):
 
     return read_info
 
+# Get command-line arguments (excluding first and last)
 in_log_list = sys.argv[1:-1]
+# Get the name of the output file
 read_count_report_out = sys.argv[-1]
 
+# Parse each log file and extract read information
 with open(read_count_report_out, 'w') as fout:
     for logfile in in_log_list:
         read_info = get_read_info(logfile)
